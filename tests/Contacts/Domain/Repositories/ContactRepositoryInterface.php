bfdb39b5421df1d7a9ae75ad51791642edd2748d
Edeno Scherer <edenoscherer@gmail.com>
2022-07-05 16:07:33 -0300

    feat: add repository interface from entities

---
+++ tests/Contacts/Domain/Repositories/ContactRepositoryInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Edeno\TestPhpCleanContactList\Contacts\Domain\Repositories;
+
+use Edeno\PhpCleanContactList\Contacts\Domain\Entities\ContactEntity;
+
+interface ContactRepositoryInterface
+{
+
+    /**
+     * @return ContactEntity[]
+     */
+    public function all(): array;
+
+    /**
+     * @param integer $id
+     * @return ContactEntity|null
+     */
+    public function getById(int $id): ?ContactEntity;
+
+    /**
+     * @param integer $idPerson
+     * @return ContactEntity[]
+     */
+    public function getByIdPerson(int $idPerson): array;
+
+    /**
+     * @param ContactEntity $contact
+     * @return ContactEntity
+     */
+    public function add(ContactEntity &$contact): ContactEntity;
+    /**
+     * @param ContactEntity $contact
+     * @return ContactEntity
+     */
+    public function update(ContactEntity &$contact): ContactEntity;
+    /**
+     * @param integer $id
+     * @return boolean
+     */
+    public function deleteById(int $id): bool;
+}
---
+++ tests/Contacts/Domain/Repositories/PersonRepositoryInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Edeno\TestPhpCleanContactList\Contacts\Domain\Repositories;
+
+use Edeno\PhpCleanContactList\Contacts\Domain\Entities\PersonEntity;
+
+interface PersonRepositoryInterface
+{
+
+    /**
+     * @return PersonEntity[]
+     */
+    public function all(): array;
+
+    /**
+     * @param integer $id
+     * @return PersonEntity|null
+     */
+    public function getById(int $id): ?PersonEntity;
+    /**
+     * @param PersonEntity $person
+     * @return PersonEntity
+     */
+    public function add(PersonEntity &$person): PersonEntity;
+    /**
+     * @param PersonEntity $person
+     * @return PersonEntity
+     */
+    public function update(PersonEntity &$person): PersonEntity;
+    /**
+     * @param integer $id
+     * @return boolean
+     */
+    public function deleteById(int $id): bool;
+}
